4658cb52c0001dc85a79fbf39c6b1232a79be813
miguel <miortiz1@uc.cl>
2023-04-25 16:32:59 -0400

    final

--- main.c
+++ main.c
@@ -16,7 +16,6 @@ int cmpfunc(const void *a, const void *b) {
 
 EMSCRIPTEN_KEEPALIVE
 int *assign_tasks(int N, int M, int *Times) {
-  
   qsort(Times, N, sizeof(int), cmpfunc);
   
   Cluster clusters[M];
@@ -27,6 +26,8 @@ int *assign_tasks(int N, int M, int *Times) {
     clusters[i].tasks_count = 0;
   }
 
+  int *clusterTasks = (int *)calloc(M * N, sizeof(int));
+
   for (int i = 0; i < N; i++) {
     int min = 0;
     for (int j = 0; j < M; j++) {
@@ -34,24 +35,25 @@ int *assign_tasks(int N, int M, int *Times) {
         min = j;
       }
     }
-    clusters[min].tasks[clusters[min].tasks_count++] = Times[i];
+    
+    // clusters[min].tasks[clusters[min].tasks_count++] = Times[i];
+    clusterTasks[min * N + clusters[min].tasks_count++] = Times[i];
     clusters[min].time += Times[i];
   }
 
   // Allocate 2D array to hold cluster tasks
   // int *clusterTasks = (int *)malloc(M * N * sizeof(int));
-  int *clusterTasks = (int *)calloc(M * N, sizeof(int));
-  for (int i = 0; i < M; ++i) {
-    for (int j = 0; j < clusters[i].tasks_count; ++j) {
-      clusterTasks[i * N + j] = clusters[i].tasks[j];
-    }
-  }
+  
+  // for (int i = 0; i < M; ++i) {
+  //   for (int j = 0; j < clusters[i].tasks_count; ++j) {
+  //     clusterTasks[i * N + j] = clusters[i].tasks[j];
+  //   }
+  // }
 
   // Free allocated memory in Cluster structs
   for (int i = 0; i < M; ++i) {
     free(clusters[i].tasks);
   }
-
   // Return array of arrays containing cluster tasks
   return clusterTasks;
 }
